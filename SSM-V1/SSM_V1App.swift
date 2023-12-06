85ec91b45553e82afacf42bfb2db7fc5749d74d5
HawkinghisDog <finn@thuernau.eu>
2023-12-06 20:08:21 +0100

    Initial Commit

---
+++ SSM-V1/SSM_V1App.swift
@@ -0,0 +1,20 @@
+//
+//  SSM_V1App.swift
+//  SSM-V1
+//
+//  Created by Waldorfschule Berlin Mitte on 06.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SSM_V1App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ SSM-V1UITests/SSM_V1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SSM_V1UITestsLaunchTests.swift
+//  SSM-V1UITests
+//
+//  Created by Waldorfschule Berlin Mitte on 06.12.23.
+//
+
+import XCTest
+
+final class SSM_V1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
