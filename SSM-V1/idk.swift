8baa15e31ee44d27fa23691c410facc6f8668122
HawkinghisDog <finn@thuernau.eu>
2023-12-06 20:40:35 +0100

    New File

---
+++ SSM-V1/idk.swift
@@ -0,0 +1,18 @@
+//
+//  idk.swift
+//  SSM-V1
+//
+//  Created by Waldorfschule Berlin Mitte on 06.12.23.
+//
+
+import SwiftUI
+
+struct idk: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    idk()
+}
